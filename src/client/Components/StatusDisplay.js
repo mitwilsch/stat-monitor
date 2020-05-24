f71e28f4874368e8c395544c405ecac9b5d6018b
Mitchell Schooler <mitchell@schooler.me>
2020-05-24 12:35:08 -0700

    Cleaned up display

--- src/client/Components/StatusDisplay.js
+++ src/client/Components/StatusDisplay.js
@@ -1,5 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Typography, Card } from '@material-ui/core';
+import {
+  Typography,
+  Card,
+  CardContent,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+} from '@material-ui/core';
+
+import { Memory, Storage, Schedule, ArrowUpward } from '@material-ui/icons';
 
 const formatBytes = (bytes, decimals = 2) => {
   if (bytes === 0) return '0 Bytes';
@@ -14,14 +24,12 @@ const formatBytes = (bytes, decimals = 2) => {
 };
 
 const Item = props => {
-  const { name, data } = props;
+  const { name, data, icon } = props;
   return (
-    <div>
-      <Typography variant="h5" gutterBottom>
-        {name}
-      </Typography>
-      <Typography variant="h3">{data}</Typography>
-    </div>
+    <ListItem>
+      <ListItemAvatar>{icon}</ListItemAvatar>
+      <ListItemText primary={name} secondary={data} />
+    </ListItem>
   );
 };
 const StatusDisplay = props => {
@@ -41,27 +49,30 @@ const StatusDisplay = props => {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <Item name="System Time" data={systemTime} />
+      <List>
+        <Item name="System Time" data={systemTime} icon={<Schedule />} />
 
-      <Item name="Uptime" data={upTime} />
-      <Item name="Total Memory" data={formatBytes(memTotal)} />
-      <Item name="Used Memory" data={formatBytes(memTotal - memFree)} />
-      <Item name="Free Memory" data={formatBytes(memFree)} />
+        <Item name="Uptime" data={upTime} icon={<ArrowUpward />} />
+        <Item
+          name="Total Memory"
+          data={formatBytes(memTotal)}
+          icon={<Memory />}
+        />
+        <Item name="Used Memory" data={formatBytes(memTotal - memFree)} />
+        <Item name="Free Memory" data={formatBytes(memFree)} />
 
-      {console.log(cpus)}
+        {console.log(cpus)}
 
-      <Typography variant="h5" gutterBottom>
-        Disk Usage
-      </Typography>
-      <Typography variant="h3">
-        {formatBytes(diskUsedSpace)} of {formatBytes(diskTotalSpace)} (
-        {formatBytes(diskAvailSpace)} available)
-      </Typography>
-
-      <Typography variant="h5" gutterBottom>
-        Disk Usage %
-      </Typography>
-      <Typography variant="h3">{diskUsagePercent}</Typography>
+        <Item
+          name="Disk Usage"
+          data={`${formatBytes(diskUsedSpace)} of ${formatBytes(
+            diskTotalSpace
+          )} (
+        ${formatBytes(diskAvailSpace)} available)`}
+          icon={<Storage />}
+        />
+        <Item name="Disk Usage %" data={diskUsagePercent} />
+      </List>
     </div>
   );
 };
